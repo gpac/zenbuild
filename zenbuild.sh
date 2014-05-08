6fdebd5174992f7b35ede576a8dfd8d4061bc1f4
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2014-05-08 17:43:37 +0200

    Move checkForCommonBuildTools out of libav/ffmpeg specific file

--- zenbuild.sh
+++ zenbuild.sh
@@ -327,5 +327,108 @@ function check_for_crosschain {
   fi
 }
 
+function checkForCommonBuildTools {
+  if isMissing "pkg-config"; then
+    echo "pkg-config not installed.  Please install with:"
+    echo "pacman -S pkgconfig"
+    echo "or"
+    echo "apt-get install pkg-config"
+    exit 1
+  fi
+
+  if isMissing "patch"; then
+    echo "patch not installed.  Please install with:"
+    echo "pacman -S patch"
+    echo "or"
+    echo "apt-get install patch"
+    exit 1
+  fi
+
+  if isMissing "python2"; then
+    echo "python2 not installed.  Please install with:"
+    echo "pacman -S python2"
+    echo "or"
+    echo "apt-get install python2"
+    exit 1
+  fi
+
+  if isMissing "autoreconf"; then
+    echo "autoreconf not installed."
+    exit 1
+  fi
+
+  if isMissing "libtool"; then
+    echo "libtool not installed.  Please install with:"
+    echo "pacman -S msys/libtool"
+    echo "or"
+    echo "apt-get install libtool"
+    exit 1
+  fi
+
+  if isMissing "make"; then
+    echo "make not installed.  Please install with:"
+    echo "pacman -S make"
+    echo "or"
+    echo "apt-get install make"
+    exit 1
+  fi
+
+  if isMissing "autopoint"; then
+    echo "autopoint not installed.  Please install with:"
+    echo "pacman -S gettext gettext-devel"
+    echo "or"
+    echo "apt-get install autopoint"
+    exit 1
+  fi
+
+  if isMissing "yasm"; then
+    echo "yasm not installed.  Please install with:"
+    echo "apt-get install yasm"
+    exit 1
+  fi
+
+  if isMissing "wget"; then
+    echo "wget not installed.  Please install with:"
+    echo "pacman -S msys/wget"
+    echo "or"
+    echo "apt-get install wget"
+    exit 1
+  fi
+
+  if isMissing "sed"; then
+    echo "sed not installed.  Please install with:"
+    echo "pacman -S msys/sed"
+    echo "or"
+    echo "apt-get install sed"
+    exit 1
+  fi
+
+  if isMissing "tar"; then
+    echo "tar not installed.  Please install with:"
+    echo "mingw-get install tar"
+    echo "or"
+    echo "apt-get install tar"
+    exit 1
+  fi
+
+  if isMissing "git" ; then
+    echo "git not installed.  Please install with:"
+    echo "pacman -S mingw-git"
+    echo "or"
+    echo "apt-get install git"
+    exit 1
+  fi
+}
+
+function get_arch {
+  host=$1
+  echo $host | sed "s/-.*//"
+}
+
+function get_os {
+  host=$1
+  echo $host | sed "s/.*-//"
+}
+
 main "$@"
 
