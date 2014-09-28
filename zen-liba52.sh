5453eb700dc31d90abb3a78b5cdfa20d0f492357
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2014-09-28 22:40:35 +0200

    Add support for AC3 decoding in VLC

--- zen-liba52.sh
+++ zen-liba52.sh
@@ -14,22 +14,17 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function build_vlc {
+function build_liba52 {
   host=$1
   pushDir $WORK/src
 
-  lazy_download "vlc.tar.xz" "http://download.videolan.org/pub/videolan/vlc/2.1.5/vlc-2.1.5.tar.xz"
-  lazy_extract "vlc.tar.xz"
+  lazy_download "liba52.tar.xz" "http://liba52.sourceforge.net/files/a52dec-0.7.4.tar.gz"
+  lazy_extract "liba52.tar.xz"
 
-  mkdir -p vlc/build/$host
-  pushDir vlc/build/$host
-  CFLAGS+=" -I$PREFIX/$host/include " \
-  LDFLAGS+=" -L$PREFIX/$host/lib " \
+  mkdir -p liba52/build/$host
+  pushDir liba52/build/$host
   ../../configure \
     --host=$host \
-    --disable-mad \
-    --disable-lua \
-    --disable-libgcrypt \
     --prefix=$PREFIX/$host
   $MAKE
   $MAKE install
@@ -38,8 +33,7 @@ function build_vlc {
   popDir
 }
 
-function vlc_get_deps {
-  echo "ffmpeg"
-  echo "liba52"
+function liba52_get_deps {
+  local a=0
 }
 
