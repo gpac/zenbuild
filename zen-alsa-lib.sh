a8cdc862621e94bbe6129ba70fdb5ca34a101948
Badr <badri.badre@gmail.com>
2015-01-09 00:25:40 +0100

    lib xcb and its dependencies

---
+++ zen-alsa-lib.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Copyright (C) 2014 - Badr BADRI
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+function alsa-lib_get_deps {
+  local a=0
+}
+
+function build_alsa-lib {
+ 
+  host=$1
+  pushDir $WORK/src
+
+  lazy_download "alsa-lib.tar.gz" "ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.0.28.tar.bz2"
+
+  lazy_extract "alsa-lib.tar.gz" 
+
+  autoconf_build $host "alsa-lib"
+
+  popDir
+
+}
---
+++ zen-xcb-proto.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Copyright (C) 2014 - Badr BADRI 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+function xcb-proto_get_deps {
+  local a=0
+}
+
+function build_xcb-proto {
+  
+  local host=$1
+  pushDir $WORK/src
+
+
+  lazy_download "xcb-proto.tar.gz" "http://xcb.freedesktop.org/dist/xcb-proto-1.11.tar.gz"
+
+  lazy_extract "xcb-proto.tar.gz"
+
+  autoconf_build $host "xcb-proto"
+
+  popDir
+
+}
