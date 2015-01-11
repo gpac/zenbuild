c5d893980a084d348d2256099e6690d2acd1e3fd
Badr <badri.badre@gmail.com>
2015-01-12 00:03:30 +0100

    add libsdl

---
+++ zen-libsdl.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Copyright (C) 2014 - Badr BADRI 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+function libsdl_get_deps {
+  local a=0
+}
+
+function build_libsdl {
+  
+  local host=$1
+  pushDir $WORK/src
+
+  lazy_download "libsdl.tar.gz" "https://www.libsdl.org/release/SDL2-2.0.3.tar.gz"
+  lazy_extract "libsdl.tar.gz" 
+
+ autoconf_build $host "libsdl" 
+
+ popDir
+
+}  
