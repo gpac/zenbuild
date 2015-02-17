5034cd62a90164e2327a947fac9871262289b8c9
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2015-02-17 22:10:55 +0100

    Fix cross-compilation to mingw

--- zen-openh264.sh
+++ zen-openh264.sh
@@ -15,30 +15,33 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-function openh264_get_deps {
-  local a=0
-}
-
 function openh264_build {
 
   host=$1
   pushDir $WORK/src
   local ARCH=$(get_arch $host)
   local OS=$(get_os $host)
- 
-  lazy_git_clone "https://github.com/cisco/openh264" "openh264" 7f967f6fc46290794da02319470afe27e7ed7a6e
-  
+
+  lazy_download "openh264.tar.gz" "https://github.com/cisco/openh264/archive/v1.3.1.tar.gz"
+  lazy_extract "openh264.tar.gz"
+  mkgit "openh264"
+
   pushDir openh264
-  
+
   sed -i "s@^PREFIX=.*@PREFIX=$PREFIX/$host@" Makefile
   sed -i "s@^ARCH=.*@ARCH=$ARCH@" Makefile
   sed -i "s@^OS=.*@OS=$OS@" Makefile
   sed -i "s/gnu/linux/" Makefile
-  sed -i "s/mingw32/msvc/" Makefile
+  sed -i "s/mingw32/mingw_nt/" Makefile
 
   $MAKE
   $MAKE install
-  
+
   popDir
   popDir
 }
+
+function openh264_get_deps {
+  local a=0
+}
+
