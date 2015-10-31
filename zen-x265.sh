f85ca6ca8fa874bb5b0e16cb20765ad17f34e975
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2015-10-31 21:40:38 +0100

    try to circumvent x265 cmake version check to build withtravisCI

--- zen-x265.sh
+++ zen-x265.sh
@@ -32,6 +32,7 @@ function x265_build {
 
   pushDir x265/
   applyPatch $scriptDir/patches/x265_02_version.diff
+  applyPatch $scriptDir/patches/x265_03_cmakeVersion.diff
 
   mkdir -p bin/$host
   pushDir bin/$host
